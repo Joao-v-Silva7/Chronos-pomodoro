20fd753e82417ddc592d6f3bdb95f90dd67bc57e
Joao Victor <Jv2806633@gmail.com>
2026-01-12 19:59:19 -0300

    feat: importando e usando arquivos de CSS Modules com escopo de componente

---
+++ chronos-pomodoro/src/App.tsx
@@ -0,0 +1,12 @@
+import { Heading } from './components/Heading';
+import './styles/theme.css';
+import './styles/global.css'
+
+export function App(){
+    return (
+        <>
+        <Heading/>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto incidunt eum, recusandae illum dolore eligendi mollitia voluptatum distinctio laudantium aliquam. Nobis tempore numquam cum, laudantium rerum recusandae praesentium consequatur et.</p>
+        </>
+    )
+}

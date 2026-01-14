8c62fcbe6cace1304c31053121ef76fcf6145378
Joao Victor <Jv2806633@gmail.com>
2026-01-13 22:18:55 -0300

    feat: Organizando componentes em pastas padronizadas

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,20 +1,19 @@
-import { Heading } from './components/Heading';
 import './styles/theme.css';
 import './styles/global.css'
-import { TimerIcon } from 'lucide-react';
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
+
 
 export function App(){
     return (
         <>
-        <Heading>
-            Olá Mundo
-            <button>
-                <TimerIcon/>
-            </button>
-            </Heading>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto incidunt eum, recusandae illum dolore eligendi mollitia voluptatum distinctio laudantium aliquam. Nobis tempore numquam cum, laudantium rerum recusandae praesentium consequatur et.
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore doloribus rem reprehenderit, at reiciendis ullam expedita in, dolore autem facilis minima aspernatur vel soluta, quod magnam deleniti aut dolor itaque!
-        </p>
+        <Container>
+            <Heading>LOGO</Heading>
+        </Container>
+
+        <Container>
+            <Heading>MENU</Heading>
+        </Container>
         </>
     )
 }

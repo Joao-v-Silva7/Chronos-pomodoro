1051355b365ec0f2b900c27ba50a621f80d3eff8
Joao Victor <Jv2806633@gmail.com>
2026-02-05 20:36:58 -0300

    feat(state): Estado global da aplicação com useState

--- chronos-pomodoro/src/models/TaskStateModel.ts
+++ chronos-pomodoro/src/models/TaskStateModel.ts
@@ -1,11 +1,13 @@
 import type { TaskModel } from "./TaskModel"
 
+//Estado -> Componente -> Filhos
+
 export type TaskStateModel = {
     task: TaskModel[];
     secondsRemaining: number;
     formattedSecondsRemaining: string;
     activeTask: TaskModel | null;
-    currentCycle: number; // 1 a 8
+    currentCycle: number; // 1 a 8 
     config: {
         workTime: number;
         shortBreakTime: number;

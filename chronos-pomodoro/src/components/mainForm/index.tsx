3a28fa5785bb965f90c886f7633c1faa5b24d75a
Joao Victor <Jv2806633@gmail.com>
2026-02-17 00:08:54 -0300

    Organizando o contexto em arquivos separados

--- chronos-pomodoro/src/components/mainForm/index.tsx
+++ chronos-pomodoro/src/components/mainForm/index.tsx
@@ -2,22 +2,11 @@ import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
-import { useTaskContext } from "../../contexts/TaskContext";
 
 export function MainForm (){
-    const {setState} = useTaskContext();
 
-    function handleclick(){
-        setState(prevState=> {
-            return {
-                ...prevState,
-                formattedSecondsRemaining: '21:00',
-            }
-        })
-    }
     return(
         <form action="" className="form">
-            <button onClick={handleclick} type="button">Clicar</button>
                 <div className="formRow">
                     <DefaultInput 
                     labelText='task' 

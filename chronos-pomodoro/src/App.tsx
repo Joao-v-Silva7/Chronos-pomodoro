18ae404128d6a499bf2b08def10764641db412b9
Joao Victor <Jv2806633@gmail.com>
2026-02-22 08:56:01 -0300

    feat(main-form): Validar o formulário e criar uma nova tarefa

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -3,11 +3,13 @@ import { Home } from './pages/Home';
 import './styles/theme.css';
 import './styles/global.css'
 import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
+import { Toaster } from 'react-hot-toast';
 
 export function App(){
     return (
     <TaskContextProvider>
         <Home />
+        <Toaster />
     </TaskContextProvider>
 )
 }
--- chronos-pomodoro/src/components/mainForm/index.tsx
+++ chronos-pomodoro/src/components/mainForm/index.tsx
@@ -3,14 +3,49 @@ import { Cycles } from "../cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
 import { useRef } from "react";
+import { toast } from "react-hot-toast";
+import type { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 
 export function MainForm (){
+    const { setState } = useTaskContext()
     const taskNameInput = useRef<HTMLInputElement>(null);
 
     function handleCreateNewTask(e: React.FormEvent<HTMLFormElement>){
         e.preventDefault()
+        
+        if(taskNameInput.current === null) return;
+        
+        const taskName = taskNameInput.current.value.trim();
+        
+        if(!taskName){
+            toast.error('Digite o nome da tarefa');
+            return;
+        }
 
-        console.log('deu certo')
+        const newTask: TaskModel= {
+            id: Date.now().toString(),
+            name: taskName,
+            startDate: Date.now(),
+            completeDate: null,
+            interruptDate: null,
+            duration: 1,
+            type: 'workTime'    
+        };
+
+        const secondsRemaining = newTask.duration * 60;
+
+        setState(prevState =>{
+            return {
+                ...prevState,
+                config: {...prevState.config},
+                activeTask: newTask,
+                currentCycle: 1, //conferir depois,
+                secondsRemaining,
+                formattedSecondsRemaining: '00:00',
+                task: [...prevState.task, newTask],
+            }
+        })
     }
 
     return(
